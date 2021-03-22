2bda216441e086457df56c5c74805a743e615e5b
Shawon Lodh <50014582+Shawon-Lodh@users.noreply.github.com>
2021-03-22 23:21:36 +0600

    initial commit

--- app/src/main/java/com/example/notification/MainActivity.java
+++ app/src/main/java/com/example/notification/MainActivity.java
@@ -1,14 +1,110 @@
+//package com.example.notification;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//import androidx.core.app.NotificationCompat;
+//
+//import android.app.Notification;
+//import android.app.NotificationManager;
+//import android.app.PendingIntent;
+//import android.content.Context;
+//import android.content.Intent;
+//import android.os.Build;
+//import android.os.Bundle;
+//import android.view.View;
+//import android.widget.Button;
+//
+//public class MainActivity extends AppCompatActivity {
+//    Button btNotification;
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        btNotification = findViewById(R.id.bt_notification);
+//
+//        btNotification.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String message = "This is notification Example";
+//                NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this)
+//                        .setSmallIcon(R.drawable.ic_baseline_message_24)
+//                        .setContentTitle("New Notification")
+//                        .setContentText(message)
+//                        .setAutoCancel(true);
+//                Intent intent = new Intent(MainActivity.this,NotificationActivity.class);
+//                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//                intent.putExtra("message",message);
+//
+//                PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+//                builder.setContentIntent(pendingIntent);
+//
+//                NotificationManager notificationManager = (NotificationManager)getSystemService(
+//                        Context.NOTIFICATION_SERVICE
+//                );
+//                notificationManager.notify(0,builder.build());
+//            }
+//        });
+//    }
+//}
+
+
 package com.example.notification;
+import android.app.Notification;
+//import android.support.v4.app.NotificationCompat;
+//import android.support.v4.app.NotificationManagerCompat;
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
-import android.os.Bundle;
-
+import static com.example.notification.App.CHANNEL_1_ID;
+import static com.example.notification.App.CHANNEL_2_ID;
 public class MainActivity extends AppCompatActivity {
+    private NotificationManagerCompat notificationManager;
+    private EditText editTextTitle;
+    private EditText editTextMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        notificationManager = NotificationManagerCompat.from(this);
+
+        editTextTitle = findViewById(R.id.edit_text_title);
+        editTextMessage = findViewById(R.id.edit_text_message);
+    }
+
+    public void sendOnChannel1(View v) {
+        String title = editTextTitle.getText().toString();
+        String message = editTextMessage.getText().toString();
+
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_1_ID)
+                .setSmallIcon(R.drawable.ic_one)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .build();
+
+        notificationManager.notify(1, notification);
+    }
+
+    public void sendOnChannel2(View v) {
+        String title = editTextTitle.getText().toString();
+        String message = editTextMessage.getText().toString();
+
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_2_ID)
+                .setSmallIcon(R.drawable.ic_two)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_LOW)
+                .build();
+
+        notificationManager.notify(2, notification);
     }
 }
